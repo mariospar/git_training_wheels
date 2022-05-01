2502b317e8400fbc5b7d686201cf3a163f9b8917
Marios Paraskevas <mariosparaskevas00@gmail.com>
2022-05-02 02:25:49 +0300

    🚧 build: Migrated build script from setup.py to setup.cfg >>> ⏰ 3h
    Setup.py is vulnerable to Arbitrary Code Execution during build or even runtime as it is very well portrayed in [this](https://github.com/mschwager/0wned) repo. Migration to setup.cfg and pyproject.toml is recommended after PEP 621 however some packages haven't fully transitioned to pyproject and require a setup.cfg file. Thus, I decided it is better to have them both.

--- git_training_wheels/__init__.py
+++ git_training_wheels/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
+from sys import version_info
+
+# Python 3.8 or later needed
+if version_info < (3, 8, 0, "final", 0):
+    raise SystemExit("Python 3.8 or later is required!")
+
 __version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,91 +1,15 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from os import name as os_name
-from os import path, walk
-from sys import version_info
-from typing import List
 
-from setuptools import find_packages, setup
+try:
+    from setuptools import setup as _setup
+except ImportError:
+    from ez_setup import use_setuptools
 
-name = "git_training_wheels"
+    use_setuptools()
+    from setuptools import setup as _setup
 
-rootdir = path.abspath(path.dirname(__file__))
+# setup.cfg basically installs itself! See setup.cfg for the project metadata.
+import setup.cfg
 
-# Python 3.8 or later needed
-if version_info < (3, 8, 0, "final", 0):
-    raise SystemExit("Python 3.8 or later is required!")
-
-# Build a list of all project modules
-packages = [
-    dirname.replace("/", ".")
-    for dirname, dirnames, filenames in walk(name)
-    if "__init__.py" in filenames
-]
-
-# Scripts
-scripts: List[str] = []
-for dirname, dirnames, filenames in walk("scripts"):
-    scripts.extend(
-        path.join(dirname, filename) for filename in filenames if not filename.endswith(".bat")
-    )
-
-# Provide bat executables in the tarball (always for Win)
-if os_name in {"ce", "nt"}:
-    scripts.extend(f"{script}.bat" for script in scripts[:])
-
-# Data_files (e.g. docs) need (directory, files-in-this-directory) tuples
-data_files = []
-for dirname, dirnames, filenames in walk("docs"):
-    fileslist = []
-    for filename in filenames:
-        fullname = path.join(dirname, filename)
-        fileslist.append(fullname)
-    data_files.append((f"share/{name}/{dirname}", fileslist))
-
-classifiers = [
-    "Development Status :: 1 - Planning" "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Intended Audience :: Education",
-    "Natural Language :: English",
-    "Operating System :: OS Independent",
-    "Topic :: Education",
-    "Topic :: Software Development :: Version Control :: Git"
-    "License :: OSI Approved :: MIT License",
-]
-
-extras_require = {
-    "tests": [
-        "tox >= 2.6.0",
-        "pytest >= 3.0.3",
-    ],
-    "dev": [
-        "check-manifest >= 0.35",
-        "readme-renderer >= 16.0",
-    ],
-}
-
-kw = {
-    "name": name,
-    "version": "0.1.0",
-    "description": "Get to know git from the CLI!",
-    "long_description": open(path.join(rootdir, "README.md"), encoding="utf-8").read(),
-    "long_description_content_type": "text/markdown",
-    "author": "Marios Paraskevas",
-    "author_email": "mariosparaskevas00@gmail.com",
-    "license": "MIT License",
-    "url": "https://github.com/mariospar/git_training_wheels",
-    "keywords": "git git-training-wheels git-tutorial",
-    "classifiers": classifiers,
-    "zip_safe": True,
-    "scripts": scripts,
-    "data_files": data_files,
-    "packages": find_packages(),
-    "extras_require": extras_require,
-    "install_requires": "poetry-core",
-}
-
-print(kw)
-
-if __name__ == "__main__":
-    setup(**kw)
+_setup(**setup.cfg.to_setup())
